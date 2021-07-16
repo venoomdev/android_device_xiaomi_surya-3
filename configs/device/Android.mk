d1343d6460d82fc6a1371c3d8579caa92fd683e5
Lunarixus <Nathan@lunarixus.dev>
2021-07-16 15:41:02 +0530

    surya: Remove packages at build time
    rdxzv: Renamed to "Debloat" and updated list
    Co-authored-by: rdxzv <rdxzv.dev@gmail.com>

---
+++ configs/device/Android.mk
@@ -0,0 +1,50 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Debloat
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	ConnMO \
+	DCMO \
+	DMService \
+	GCS \
+	MaestroPrebuilt \
+	Maps \
+	MicropaperPrebuilt \
+	MyVerizonServices \
+	OBDM_Permissions \
+	Ornament \
+	SCONE \
+	ScribePrebuilt \
+	Showcase \
+	Chrome \
+	Chrome-Stub \
+	SoundAmplifierPrebuilt \
+	SprintDM \
+	SprintHM \
+	Flipendo \
+	Tycho \
+	USCCDM \
+	VZWAPNLib \
+	VzwOmaTrigger \
+	WallpapersBReel2020 \
+	arcore \
+	obdm_stub \
+	DevicePolicyPrebuilt \
+	RecorderPrebuilt \
+	PrebuiltGmail \
+	YouTube \
+	YouTubeMusicPrebuilt \
+	Drive \
+	TurboPrebuilt \
+	SafetyHubPrebuilt \
+	Stk \
+	FM2
+	
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
