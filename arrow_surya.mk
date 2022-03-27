1ec02a773616a52fd3855d1d5e90b8e2a3608198
rdxzv <rdxzv.dev@gmail.com>
2022-03-27 18:07:16 -0300

    surya: Arrow bringup

--- arrow_surya.mk
+++ arrow_surya.mk
@@ -19,10 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
+
+# GMS
+ARROW_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # Targets
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -30,7 +34,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_surya
+PRODUCT_NAME := arrow_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
