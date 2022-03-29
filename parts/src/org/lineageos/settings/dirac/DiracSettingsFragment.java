lostark13/device_xiaomi_surya

surya: parts: Add MiSound scenes
Signed-off-by: Lostark13 <tushar9915@gmail.com>

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -36,11 +36,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
+    private static final String PREF_SCENE = "scenario_selection";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
+    private ListPreference mScenes;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -60,6 +62,10 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
         mPreset.setEnabled(enhancerEnabled);
+
+        mHeadsetType = (ListPreference) findPreference(PREF_SCENE);
+        mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -70,6 +76,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
                 return true;
             case PREF_PRESET:
                 DiracUtils.setLevel(String.valueOf(newValue));
+                return true;
+            case PREF_SCENE:
+                DiracUtils.setScenario(Integer.parseInt(newValue.toString()));
                 return true;
             default:
                 return false;
@@ -84,5 +93,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
     }
 }

venoomdev/android_device_xiaomi_surya-3

Signed-off-by: rdxzv <rdxzv.dev@gmail.com>

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -38,11 +38,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
+    private static final String PREF_SCENE = "scenario_selection";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
+    private ListPreference mScenes;
     private DiracUtils mDiracUtils;
 
     @Override
@@ -67,6 +69,10 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
         mPreset.setEnabled(enhancerEnabled);
+
+        mScenes = (ListPreference) findPreference(PREF_SCENE);
+        mScenes.setOnPreferenceChangeListener(this);
+        mScenes.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -79,6 +85,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
             case PREF_PRESET:
                 mDiracUtils.setLevel((String) newValue);
                 return true;
+            case PREF_SCENE:
+                DiracUtils.setScenario(Integer.parseInt(newValue.toString()));
+                return true;    
             default: return false;
         }
     }
@@ -91,5 +100,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mDiracUtils.setEnabled(isChecked);
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
     }
 }