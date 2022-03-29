GtrCraft/device_xiaomi_surya

surya: Declare support for ARMv8.2 extensions
The ARMv8.2 CRC32 optimizations in zlib-ng fail to build if the
architecture is not set to armv8-2a.

Change-Id: I0d9f64ef4e676a5f8de93f87e48bf02f48af07af

BoardConfig.mk
@@ -23,7 +23,7 @@ include vendor/xiaomi/surya/BoardConfigVendor.mk
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic

venoomdev/android_device_xiaomi_surya-3

BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76